827bf9cc551616c2b96742aa3fe860292ba9ca14
Mukesh Otwani <mukeshotwani.50@gmail.com>
2025-10-17 20:55:13 +0530

    Update AddToCart.spec.js

--- tests/AddToCart/AddToCart.spec.js
+++ tests/AddToCart/AddToCart.spec.js
@@ -4,3 +4,18 @@ test("Sample Test For Github Branching",async function ({page})
 {
     
 })
+
+test("Sample Test For Github Branching1",async function ({page}) 
+{
+    
+})
+
+test("Sample Test For Github Branching2",async function ({page}) 
+{
+    
+})
+
+test("Sample Test For Github Branching3",async function ({page}) 
+{
+    
+})
