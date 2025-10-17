394d426d099c83d15c0bd18e5f266ddb98bcc52f
Mukesh Otwani <mukeshotwani.50@gmail.com>
2025-10-17 20:54:32 +0530

    added login steps in add to cart testcases

--- tests/AddToCart/AddToCart.spec.js
+++ tests/AddToCart/AddToCart.spec.js
@@ -2,5 +2,10 @@ import {test,expect} from "@playwright/test"
 
 test("Sample Test For Github Branching",async function ({page}) 
 {
+
+    await page.goto("https://freelance-learn-automation.vercel.app/login")
+    await page.getByPlaceholder("Enter Email").fill("admin@email.com")
+    await page.getByPlaceholder("Enter Password").fill("admin@123")
+    await page.getByText("Sign in").nth(1).click()
     
 })
